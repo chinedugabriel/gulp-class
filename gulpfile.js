688750836709aef24eb526638390bc80225ffec0
chinedugabriel <chinedugabriel29@gmail.com>
2021-11-11 19:43:07 +0100

    Defined other dependencies within workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 const { src, dest, watch } = require('gulp');
-const sass = require('gulp-sass');
+const minifyCSS = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 const compileCSS = () => {
     return src('./assets/src/**/*.scss')
     .pipe(sass())
-    .pipe(dest('./assets/dist'));
+    .pipe(minifyCSS())
+    .pipe(dest('./assets/dist'))
+    .pipe(browserSync.stream());
+}
+
+const devWatch = () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    watch('./assets/src/**/*.scss', compileCSS);
+    watch('./index.html').on('change', browserSync.reload);
 }
 
 exports.sass = compileCSS;
+exports.watch = devWatch;
